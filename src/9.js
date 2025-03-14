9cd383d770b6d6d234bcb33b230e1357b1124f0b
hamstracelius840 <hamstracelius840@gmail.com>
2025-03-14 18:31:25 +0000

    Add 9.js

---
+++ src/9.js
@@ -0,0 +1,7 @@
+
+function getRandomColor() {
+  var colors = ["red", "orange", "yellow", "green", "blue", "purple"];
+  return colors[Math.floor(Math.random() * colors.length)];
+}
+
+console.log(getRandomColor());
